a25935b60a2c41f005b9cc4eeddbdb76810a6ef1
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-05-26 09:44:25 +0300

    day7 Completed

--- day5.py
+++ day5.py
@@ -1,9 +1,6 @@
 # 1. Write a Python program to count the number of characters (character frequency) in a string. Sample String : google.com' Expected Result : {'g': 2, 'o': 3, 'l': 1, 'e': 1, '.': 1, 'c': 1, 'm': 1}
 # 2. Write a Python program to get a string made of the first 2 and last 2 characters of a given string. If the string length is less than 2, return the empty string instead. Sample String : 'w3resource' Expected Result : 'w3ce' Sample String : 'w3' Expected Result : 'w3w3' Sample String : ' w' Expected Result : Empty String
 # 3. Write a Python program to add 'ing' at the end of a given string (length should be at least 3). If the given string already ends with 'ing', add 'ly' instead. If the string length of the given string is less than 3, leave it unchanged. Sample String : 'abc' Expected Result : 'abcing' Sample String : 'string' Expected Result : 'stringly'
-# 4. Write a Python program to remove duplicates from a list.
-# 5. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] Expected Output : ['Green', 'White', 'Black']
-# 6. Write a Python program to check if each number is prime in a given list of numbers. Return True if all numbers are prime otherwise False. Sample Data: ([0, 3, 4, 7, 9]) -> False ([3, 5, 7, 13]) -> True ([1, 5, 3]) -> False
 
 def character_frequency_counter(string):
     character_frequency = {}
@@ -24,17 +21,5 @@ def adding_suffixes(string):
         print(string + "ly")
     else:
         print(string + "ing")
-
-def remove_duplicates(lst):
-    print(list(set(lst)))
-
-def remove_elements(lst):
-    indices_to_remove = [0, 4, 5]
-    updated_lst = [item for i, item in enumerate(lst) if i not in indices_to_remove]
-    print(updated_lst)
-
-# def prime_number(numbers):
-#     if numbers < 2:
-#         print(False)
     
     
--- day6.py
+++ day6.py
@@ -1,26 +1,15 @@
-# 1. Write a Python program to combine two dictionary by adding values for common keys. d1 = {'a': 100, 'b': 200, 'c':300} d2 = {'a': 300, 'b': 200, 'd':400} Sample output: Counter({'a': 400, 'b': 400, 'd': 400, 'c': 300})
-# 2. Write a Python program to create a dictionary from a string. Note: Track the count of the letters from the string. Sample string : 'w3resource' Expected output: {'w': 1, '3': 1, 'r': 2, 'e': 2, 's': 1, 'o': 1, 'u': 1, 'c': 1}
-# 3. Write a Python program to sort Counter by value. Sample data : {'Math':81, 'Physics':83, 'Chemistry':87} Expected data: [('Chemistry', 87), ('Physics', 83), ('Math', 81)]
+# 4. Write a Python program to remove duplicates from a list.
+# 5. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] Expected Output : ['Green', 'White', 'Black']
+# 6. Write a Python program to check if each number is prime in a given list of numbers. Return True if all numbers are prime otherwise False. Sample Data: ([0, 3, 4, 7, 9]) -> False ([3, 5, 7, 13]) -> True ([1, 5, 3]) -> False
 
-#1
-from collections import Counter
+def remove_duplicates(lst):
+    print(list(set(lst)))
 
-d1 = {'a': 100, 'b': 200, 'c': 300}
-d2 = {'a': 300, 'b': 200, 'd': 400}
+def remove_elements(lst):
+    indices_to_remove = [0, 4, 5]
+    updated_lst = [item for i, item in enumerate(lst) if i not in indices_to_remove]
+    print(updated_lst)
 
-combined_dict = Counter(d1) + Counter(d2)
-print(combined_dict)
-
-#2
-from collections import Counter
-
-string = 'Flabbergasted'
-letter_count = Counter(string)
-print(letter_count)
-
-#3
-from collections import Counter
-
-data = {'Math': 81, 'Physics': 83, 'Chemistry': 87}
-sorted_data = sorted(data.items(), key=lambda x: x[1], reverse=True)
-print(sorted_data)
+# def prime_number(numbers):
+#     if numbers < 2:
+#         print(False)
